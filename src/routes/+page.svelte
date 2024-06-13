11946105844a0dfabfa9f9a593ef73099d64cba4
Gilberdi Sinaga <gilberdising012@gmail.com>
2024-06-14 01:48:11 +0700

    feat: range slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<svelte:window on:change={squareOperation} />
+<svelte:window on:change={squareOperation, showSliderValueCol, showSliderValueRow} />
 
 <svelte:head>
 	<link rel="icon" type="image/x-icon" href={favicon}>
@@ -8,7 +8,7 @@
   import { onMount } from "svelte";
 
   let favicon = 'lb-dark.svg';
-  let title, director, rating, square, chart, dlchart, loading, wait, col, row, user;
+  let title, director, rating, square, chart, dlchart, loading, wait, col, row, user, note;
 
   onMount(() => {
     let matcher;
@@ -22,6 +22,18 @@
 
     matcher = window.matchMedia("(prefers-color-scheme: dark)");
     matcher.addListener(onUpdate);
+
+    const slider_input = document.getElementById('slider_input');
+    const slider_input_row = document.getElementById('slider_input_row');
+
+    showSliderValueCol();
+    window.addEventListener("resize",showSliderValueCol);
+    slider_input.addEventListener('input', showSliderValueCol, false);
+
+    showSliderValueRow();
+    window.addEventListener("resize",showSliderValueRow);
+    slider_input_row.addEventListener('input', showSliderValueRow, false);
+
     onUpdate();
     squareOperation();
   })
@@ -42,20 +54,32 @@
   
   const squareOperation = () => {
     let innerElem = "";
-    const colValue = parseInt(col.value);
-    const rowValue = parseInt(row.value);
+    let colValue = parseInt(col.value);
+    let rowValue = parseInt(row.value);
+
+    if (rowValue * colValue > 49) {
+      note.hidden = false;
+    } else {
+      note.hidden = true;
+    }
 
    for (let i = 0; i < colValue * rowValue; i++) {
       square.style["grid-template-columns"] = `repeat(${colValue}, 1fr)`;
       square.style["grid-template-rows"] = `repeat(${rowValue}, 1fr)`;
-      innerElem += `<div class="grid-item">` + innerElement() + `</div> `;
+      if (colValue > 7 || rowValue > 7) {
+        innerElem += `<div class="grid-item">` + `</div> `;
+      } else {
+        innerElem += `<div class="grid-item">` + innerElement() + `</div> `;
+      }
     }
     square.innerHTML = innerElem;
 
     const gridItems = document.querySelectorAll('.grid-item');
-    gridItems.forEach((gi) => {
-      gi.innerHTML = innerElement();
-    });
+    if (colValue <= 7 && rowValue <= 7) {
+      gridItems.forEach((gi) => {
+        gi.innerHTML = innerElement();
+      });
+    }
   };
 
   const getChart = () => {
@@ -75,6 +99,7 @@
 
     loading.className = 'text-secondary';
     loading.hidden = false;
+    loading.innerHTML = 'Loading';
     
     const loadingOperation = window.setInterval(() => {
       if (wait.innerHTML.length > 3) 
@@ -105,6 +130,38 @@
       clearInterval(loadingOperation);
     })
   }
+
+  function showSliderValueCol() {
+    slider_thumb.innerHTML = slider_input.value;
+    let bulletPosition;
+    if (slider_input.value == 1) {
+      bulletPosition = 0;
+    } else {
+      bulletPosition = (slider_input.value / slider_input.max);
+    }
+    const space = slider_input.offsetWidth - slider_thumb.offsetWidth;
+
+    slider_thumb.style.left = (bulletPosition * space) + 'px';
+    slider_line.style.width = slider_input.value + '%';
+
+    squareOperation();
+  }
+
+  function showSliderValueRow() {
+    slider_thumb_row.innerHTML = slider_input_row.value;
+    let bulletPosition;
+    if (slider_input_row.value == 1) {
+      bulletPosition = 0;
+    } else {
+      bulletPosition = (slider_input_row.value / slider_input_row.max);
+    }
+    const space = slider_input_row.offsetWidth - slider_thumb_row.offsetWidth;
+
+    slider_thumb_row.style.left = (bulletPosition * space) + 'px';
+    slider_line_row.style.width = slider_input_row.value + '%';
+
+    squareOperation();
+  }
 </script>
 
 <div class="container my-5 justify-content-evenly d-flex flex-wrap h-100">
@@ -112,26 +169,28 @@
     <div>
       <h1 class="mb-4"><span class="letterboxd">Letterboxd</span> collage generator</h1>
       <form on:submit={getChart}>
-        <div class="mb-3">
+        <div class="mb-2">
           <input class="form-control mb-1" style="width: 200px; display: inline-block;" bind:this={user} type="text" placeholder="Letterboxd username" required>
-          <div>
-            <select class="form-control" style="width: 45px; display: inline-block;" bind:this={col}>
-              <option value="2">2</option>
-              <option selected value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-            </select>
-            ×
-            <select class="form-control" style="width: 45px; display: inline-block;" bind:this={row}>
-              <option value="2">2</option>
-              <option selected value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-            </select>
+          <div class="mt-3">
+            <b>Column:</b>
+            <div class="range-slider">
+              <div id="slider_thumb" class="range-slider_thumb"></div>
+              <div class="range-slider_line">
+                <div id="slider_line" class="range-slider_line-fill"></div>
+              </div>
+              <input id="slider_input" class="range-slider_input" type="range" value="3" min="1" max="16" bind:this={col}>
+            </div>
+            <b>Row:</b>
+            <div class="range-slider">
+              <div id="slider_thumb_row" class="range-slider_thumb"></div>
+              <div class="range-slider_line">
+                <div id="slider_line_row" class="range-slider_line-fill"></div>
+              </div>
+              <input id="slider_input_row" class="range-slider_input" type="range" value="3" min="1" max="16" bind:this={row}>
+            </div>
+            <div bind:this={note} hidden class="mb-2">
+              <i class="text-danger">Note: This may take longer to load...</i>
+            </div>
           </div>
         </div>
         <input class="btn btn-primary mb-2" type="submit" value="Submit">
